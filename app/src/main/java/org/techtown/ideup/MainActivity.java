8d7f40d0240366c6c3b42dd2d9025db453772715
Jiwon Lee <jiddoly@gmail.com>
2020-12-05 16:58:08 +0900

    gitignore 수정

---
+++ app/src/main/java/org/techtown/ideup/MainActivity.java
@@ -0,0 +1,13 @@
+package org.techtown.ideup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
