8f36705f9fc0e23fca3ee454f09c2befaf9bfd6b
overthestream <njhnjh02@naver.com>
2020-12-29 22:10:44 +0900

    Bottom Navigation

--- app/src/main/java/org/techtown/ideup/MainActivity.java
+++ app/src/main/java/org/techtown/ideup/MainActivity.java
@@ -2,11 +2,15 @@ package org.techtown.ideup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.techtown.ideup.InfoTeam.MyTeamFragment;
 import org.techtown.ideup.InfoTeam.infoTeamActivity;
@@ -25,13 +29,68 @@ public class MainActivity extends AppCompatActivity {
     private ListTeamFragment listTeamFragment = new ListTeamFragment();
     private MyTeamFragment myTeamFragment = new MyTeamFragment();
 
-    private org.techtown.ideup.InfoTeam.infoTeamActivity infoTeamActivity = new infoTeamActivity();
+//    private org.techtown.ideup.InfoTeam.infoTeamActivity infoTeamActivity = new infoTeamActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
+        fragmentManager.beginTransaction().replace(R.id.main_layout, myTeamFragment).commitAllowingStateLoss();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.tab1: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, userComplainFragment).commitAllowingStateLoss();
+         // if 창업가  getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, teamComplainFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.tab2: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, listTeamFragment).commitAllowingStateLoss();
+
+//if 창업가               getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, myTeamFragment).commitAllowingStateLoss();
+
+                        return true;
+                    }
+                    case R.id.tab3: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, projectListFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.tab4: {
+                        // 4번 버튼.?
+                        return true;
+                    }
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+
+    public void setFragment(String fragment){ // 프래그먼트 replace
+
+        if(fragment.equals("userComplain")){ // 소비자문의리스트 프래그먼트
+            fragmentManager.beginTransaction().replace(R.id.main_layout, userComplainFragment).commit();
+        }
+        else if(fragment.equals("teamComplain")){ // 창업팀에서 보는 소비자문의리스트 프래그먼트
+            fragmentManager.beginTransaction().replace(R.id.main_layout, teamComplainFragment).commit();
+        }
+        else if(fragment.equals("userComplainRegister")){ // 소비자문의 등록 프래그먼트
+            fragmentManager.beginTransaction().replace(R.id.main_layout,userComplainRegisterFragment).commit();
+        }
+        else if(fragment.equals("projectList")){ // 프로젝트 프래그먼트
+            fragmentManager.beginTransaction().replace(R.id.main_layout,projectListFragment).commit();
+        }
+        else if(fragment.equals("listTeam")){
+            fragmentManager.beginTransaction().replace(R.id.main_layout, listTeamFragment).commit();
+        }
+
+    }
+}
+        /*
         Button companyInfoBT = findViewById(R.id.companyInfoBT);
         Button userComplainBT = findViewById(R.id.userComplainBT);
         Button teamComplainBT = findViewById(R.id.teamComplainBT);
@@ -90,12 +149,12 @@ public class MainActivity extends AppCompatActivity {
         else if(fragment.equals("userComplainRegister")){ // 소비자문의 등록 프래그먼트
             fragmentManager.beginTransaction().replace(R.id.frameLayout,userComplainRegisterFragment).commit();
         }
-        else if(fragment.equals("projectList")){ // 소비자문의 등록 프래그먼트
+        else if(fragment.equals("projectList")){ // 프로젝트 프래그먼트
             fragmentManager.beginTransaction().replace(R.id.frameLayout,projectListFragment).commit();
         }
-        else if(fragment.equals("listteam")){
+        else if(fragment.equals("listTeam")){
             fragmentManager.beginTransaction().replace(R.id.frameLayout, listTeamFragment).commit();
         }
 
     }
-}
+}*/
